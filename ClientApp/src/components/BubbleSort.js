02b9a0aae85b87add5b1c67516c9ea935e956ec8
GorginZ <georgia.z.leng@gmail.com>
2020-12-30 15:00:34 +1100

    try fetch sorted data

--- ClientApp/src/components/BubbleSort.js
+++ ClientApp/src/components/BubbleSort.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import CollectionInput from "./Input";
-// import Input from "reactstrap/lib/Input";
+// import CollectionInput from "./Input";
 
 
 export class BubbleSort extends Component {
@@ -16,6 +15,7 @@ export class BubbleSort extends Component {
     const response = await fetch("bubblesort");
     const data = await response.json();
     this.setState({ collection: data });
+    console.log(this.state);
   }
 
   incrementItteration() {
@@ -28,8 +28,15 @@ export class BubbleSort extends Component {
     return (
       <>
       <div>
+        <h4>Enter a collection to sort</h4>
+        <input placeholder="Enter collection to sort eg: 0,2,5,3,2,1,3" />        
+        <button className="btn btn-primary" onClick={this.getSortedCollectionItterations}> Sort Collection 
+        </button>
+      </div>
+
+      <div>
       <h1>Bubble Sort</h1>
-      <CollectionInput></CollectionInput>
+      {/* <CollectionInput></CollectionInput> */}
 
         <p aria-live="polite">
           Current itteration: <strong>{this.state.currentItteration}</strong>
