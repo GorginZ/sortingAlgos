db2251cf99576081a159d1aefcf5c74bcd48bd15
GorginZ <georgia.z.leng@gmail.com>
2021-01-06 13:29:07 +1100

    tidying up and deleting commented out code

--- Controllers/SortingController.cs
+++ Controllers/SortingController.cs
@@ -27,35 +27,14 @@ namespace sortingAlgos.Controllers
       var collectionItterations = new Sorter(new int[] { 5, 4, 3, 2 });
       return collectionItterations.Collection;
     }
-    // var readOnlySpan = new ReadOnlySpan<byte>(jsonUtf8Bytes);
-    // var decerializedInput = JsonSerializer.Deserialize<Sorter>(readOnlySpan);
-    // return decerializedInput.ListSortedCollection;
 
     [HttpPost]
     public IEnumerable<int[]> Post([FromBody] string collection)
     {
       var sorter = new Sorter(Sorter.StringToIntArr(collection));
-      // var sorter = new Sorter(collection);
       return sorter.ListSortedCollection;
-      // var myList = new List<int[]> ();
-
-      // myList.Add(collection.InputCollection);
-      // return myList;
-      // return sorter.ListSortedCollection;
     }
 
-    // [HttpPost]
-    // public IEnumerable<int[]> Post(byte[] jsonUtf8Bytes)
-    // {
-    //   var readOnlySpan = new ReadOnlySpan<byte>(jsonUtf8Bytes);
-    //   var decerializedInput = JsonSerializer.Deserialize<JsonProperty>(readOnlySpan);
-    //   string collection = decerializedInput.ToString();
-    //   var intCollection = Sorter.StringToIntArr(collection);
-    //   var sorter = new Sorter(intCollection);
-
-
-    //   return sorter.ListSortedCollection;
-    // }
 
   }
 }
--- Program.cs
+++ Program.cs
@@ -14,13 +14,6 @@ namespace sortingAlgos
     public static void Main(string[] args)
     {
       CreateHostBuilder(args).Build().Run();
-      var sorter = new Sorter(new int[]{5,4,3,2,1,4,6,5,4,3});
-      foreach (int[] item in sorter.ListSortedCollection)
-      {
-        // Console.WriteLine(item[0].ToString());
-          Console.WriteLine(item[0].ToString() + " " + item[1].ToString() + item[2].ToString() + item[3].ToString() + item[4].ToString() + item[5].ToString() + item[6].ToString() + item[7].ToString() + item[8].ToString() + item[9].ToString());
-
-      }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
